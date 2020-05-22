1d3cd9b1a63ea3c0fc216371c6759c1e81b1053e
Pragati Verma <itispragativerma@gmail.com>
2020-05-22 18:23:16 +0530

    Added LocalStorage

--- src/App.js
+++ src/App.js
@@ -1,47 +1,56 @@
-import React, { Component } from 'react';
-import './App.css';
-import Form from './Components/Form';
-import TodoList from './Components/TodoList';
-import Header from './Components/Header';
+import React, { Component } from "react";
+import "./App.css";
+import Form from "./Components/Form";
+import TodoList from "./Components/TodoList";
+import Header from "./Components/Header";
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			tasks: ['Go to walk', 'Write Assignments', 'Make a React App'],
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: ["Go to walk", "Write Assignments", "Make a React App"]
+    };
+  }
 
-	addTask = (task) => {
-		if (task !== '' && !this.state.tasks.includes(task)) {
-			this.setState((state) => {
-				return { tasks: [...state.tasks, task] };
-			});
-		} else {
-			alert('Please Enter Correct Todo!!');
-		}
-	};
+  addTask = task => {
+    if (task !== "" && !this.state.tasks.includes(task)) {
+      const newTask = [...this.state.tasks, task];
+      localStorage.setItem("task", JSON.stringify(newTask));
+      this.setState(state => {
+        return { tasks: [...state.tasks, task] };
+      });
+    } else {
+      alert("Please Enter Correct Todo!!");
+    }
+  };
 
-	handleDelete = (index) => {
-		const newArr = [...this.state.tasks];
-		newArr.splice(index, 1);
-		this.setState({ tasks: newArr });
-	};
+  handleDelete = index => {
+    const newArr = [...this.state.tasks];
+    newArr.splice(index, 1);
+    localStorage.setItem("task", JSON.stringify(newArr));
+    this.setState({ tasks: newArr });
+  };
 
-	render() {
-		const { tasks } = this.state;
-		return (
-			<div className='App'>
-				<div className='wrapper'>
-					<div className='card frame'>
-						<Header numTodos={tasks.length} />
-						<TodoList todos={tasks} deleteTask={this.handleDelete} />
-						<Form addTask={this.addTask} />
-					</div>
-				</div>
-			</div>
-		);
-	}
+  componentDidMount() {
+    if (JSON.parse(localStorage.getItem("task"))) {
+      this.setState({ tasks: JSON.parse(localStorage.getItem("task")) });
+    }
+  }
+
+  render() {
+    const { tasks } = this.state;
+    return (
+      <div className="App">
+        <div className="wrapper">
+          <div className="card frame">
+            <Header numTodos={tasks.length} />
+            <TodoList todos={tasks} deleteTask={this.handleDelete} />
+            <Form addTask={this.addTask} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
